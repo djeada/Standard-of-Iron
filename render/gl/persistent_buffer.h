194ab3161da308001b98d01f40e2c8712d997ffb
djeada <addjellouli1@gmail.com>
2025-10-29 21:21:03 +0100

    apply format

--- render/gl/persistent_buffer.h
+++ render/gl/persistent_buffer.h
@@ -88,9 +88,8 @@ public:
       return;
     }
 
-    // Check if we have a valid OpenGL context before cleanup
     if (QOpenGLContext::currentContext() == nullptr) {
-      // No valid context, just reset state without OpenGL calls
+
       m_buffer = 0;
       m_mappedPtr = nullptr;
       m_capacity = 0;
