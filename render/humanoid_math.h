0cc42b512ce710aa8b40154b346caa96f8733e16
djeada <addjellouli1@gmail.com>
2025-12-16 17:16:40 +0100

    fix river bank visibility

--- render/humanoid_math.h
+++ render/humanoid_math.h
@@ -1,5 +1,5 @@
-// This file is deprecated. Include humanoid/humanoid_math.h instead.
-// Kept for backwards compatibility with any legacy includes.
+
+
 #pragma once
 
 #include "humanoid/humanoid_math.h"
