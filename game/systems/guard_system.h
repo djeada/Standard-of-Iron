c600305dbb718cd3f42049a10aa1a29a690a78b7
djeada <addjellouli1@gmail.com>
2025-12-17 13:49:51 +0100

    fix compilation error

--- game/systems/guard_system.h
+++ game/systems/guard_system.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include "../core/system.h"
+
 namespace Engine::Core {
 class World;
 }
 
 namespace Game::Systems {
 
-class GuardSystem {
+class GuardSystem : public Engine::Core::System {
 public:
-  static void update(Engine::Core::World *world, float delta_time);
+  GuardSystem() = default;
+  ~GuardSystem() override = default;
+
+  void update(Engine::Core::World *world, float delta_time) override;
 };
 
 } // namespace Game::Systems
